57dbf633f5930fdf50e031068a999b99280ae271
我的世界普忒头 <deutschlandSS1945@gmail.com>
2025-03-15 11:39:22 +0800

    Add files via upload

---
+++ babybusFansBBS/config/config_ucenter.php
@@ -0,0 +1,24 @@
+<?php
+
+
+define('UC_CONNECT', 'mysql');
+define('UC_STANDALONE', 0);
+
+define('UC_DBHOST', '127.0.0.1');
+define('UC_DBUSER', 'root');
+define('UC_DBPW', '');
+define('UC_DBNAME', 'babybus');
+define('UC_DBCHARSET', 'utf8mb4');
+define('UC_DBTABLEPRE', '`babybus`.pre_ucenter_');
+define('UC_DBCONNECT', 0);
+
+define('UC_AVTURL', '');
+define('UC_AVTPATH', '');
+
+define('UC_CHARSET', 'utf-8');
+define('UC_KEY', '55qeTaJ886344d7bb1MaW04cH4Za8bw9ddz0E5rc04VbqdSc8dMbecp3m2Ta6aM8');
+define('UC_API', 'http://localhost:8080/babybusFansBBS/uc_server');
+define('UC_APPID', '1');
+define('UC_IP', '');
+define('UC_PPP', 20);
+?>
